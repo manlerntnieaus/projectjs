e704c8cd59fac5cc18c983f051c56cf3766d274d
Denys <dkulykinside@gmail.com>
2020-08-10 16:11:06 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const q1 = prompt("Один из последних просмотренных фильмов?", ""),
+	  q2 = +prompt("На сколько оцените его?", ""),
+	  q3 = prompt("Один из последних просмотренных фильмов?", ""),
+	  q4 = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[q1] = q2;
+personalMovieDB.movies[q3] = q4;
+
+console.log(personalMovieDB);
